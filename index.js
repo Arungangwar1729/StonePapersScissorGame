7900105cd177a377cecb681a46b67cd0da2b51b6
Arun Gangwar <gangwararun230@gmail.com>
2023-03-23 09:42:40 +0530

    write js logic

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+let playerScore = 0;
+let computerScore = 0;
+function palyerRound(playerSelection, computerSelection) {
+  if( (playerSelection==='rock'&& computerSelection==='paper')||(playerSelection==='paper')
+)}
+
+const playerSelection = prompt(`choose between rock paper scissors`);
+const computerSelection = function getcomputerChoice() {
+  const choice = Math.trunc(Math.random() * 3) + 1;
+  if (choice === 1) {
+    return "rock";
+  } else if (choice === 2) {
+    return "paper";
+  } else choice === 3;
+  {
+    return "scissors";
+  }
+};
